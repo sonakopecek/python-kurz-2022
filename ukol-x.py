52fe852e5ef54fea6e4917e10016e2f1e858c9b4
sonakopecek <145699456+sonakopecek@users.noreply.github.com>
2023-09-24 16:30:31 +0000

    úkol 1

---
+++ ukol-x.py
@@ -0,0 +1,11 @@
+# 1 vytvoření emailové adresy
+jmeno = "sona"
+email = "@czechitas.cz"
+print (jmeno + email)
+
+# 2 bonus 
+jmeno_a_prijmeni = input("Zadej své jméno a příjmení: ")
+#všechna velká
+print (jmeno_a_prijmeni.upper())
+# všechna malá
+print (jmeno_a_prijmeni.lower())
